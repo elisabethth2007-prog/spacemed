359075d87b7c13d016243fe5c8c745e6983c981b
Elisabeth Thamm <elisabeth-sophie.thamm@charite.de>
2026-04-29 23:14:17 +0200

    added test for find_peaks and modified accordingly

--- src/spacemed/find_peaks.py
+++ src/spacemed/find_peaks.py
@@ -6,10 +6,10 @@ def find_peaks(data, w):
     peaks = []
     for i in range(len(data)):
         start = max(i - w, 0)
-        end = min(i + w, len(data))
+        end = min(i + w + 1, len(data))
         window = data[start:end]
         max_pos = numpy.argmax(window) + start
-        if i == max_pos:
+        if data[i] == max(window) and window.count(data[i]) == 1:
             peaks.append(i)
 
     return peaks
---
+++ src/tests/test_find_peaks.py
@@ -0,0 +1,27 @@
+from spacemed.find_peaks import find_peaks
+
+def test_find_peaks_simple():
+    data = [1, 3, 2, 1]
+    w = 1
+
+    peaks = find_peaks(data, w)
+
+    assert peaks == [1]
+
+
+def test_find_peaks_multiple():
+    data = [1, 3, 1, 3, 1]
+    w = 1
+
+    peaks = find_peaks(data, w)
+
+    assert peaks == [1, 3]
+
+
+def test_find_peaks_none():
+    data = [5, 4, 3, 2, 1]
+    w = 1
+
+    peaks = find_peaks(data, w)
+
+    assert peaks == [0] 
