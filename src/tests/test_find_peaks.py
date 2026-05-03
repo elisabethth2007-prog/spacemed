035fe60e36f44c5c99042599978152853962384a
Elisabeth Thamm <elisabeth-sophie.thamm@charite.de>
2026-05-03 15:54:36 +0200

    added function for global and local  maxima to test_find_peaks

--- src/tests/test_find_peaks.py
+++ src/tests/test_find_peaks.py
@@ -17,6 +17,15 @@ def test_find_peaks_multiple():
     peaks = find_peaks(data, w)
 
     assert peaks == [1, 3]
+    
+
+def test_find_peaks_long_w():
+    data = [1, 3, 4, 1, 3, 1]
+    w = 2
+
+    peaks = find_peaks(data, w)
+
+    assert peaks == [2]
 
 
 def test_find_peaks_none():
